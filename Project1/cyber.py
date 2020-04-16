7a8aa58d611b67f49d50070035d41840ece6b8d2
tgg9159 <59924851+tgg9159@users.noreply.github.com>
2020-04-16 11:48:55 +0800

    Update cyber.py

--- Project1/cyber.py
+++ Project1/cyber.py
@@ -4,7 +4,7 @@ def avatar_url():
 def product_url():
     return 'https://images-ext-2.discordapp.net/external/NrbSEa1_rLDwnfNPp0co9FmjfNfbAgjyL6GVSDyhnqY/https/assets.supremenewyork.com/186849/rs/k9sHDyYfhIs.jpg?width=450&height=450'
 def send_webhook():
-    url = 'https://discordapp.com/api/webhooks/699902394187644940/Yxx0wyI0N6B6e5ydrpw5gxsnyBSg0G2w7s0xrtFmca35Jhc7hlSoi50V4u2IeiO6ZLnU'
+    url = 'webhook'
     webhook = Webhook.from_url(url, adapter=RequestsWebhookAdapter())
     embed = Embed(title='Successfully checked out!',
                   color=Color.from_rgb(50, 205, 50),
